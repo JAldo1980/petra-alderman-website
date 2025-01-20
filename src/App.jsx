136a41f58db49e32d7dcf5bff042c1f17baa1d6e
James Alderman <james_codes@icloud.com>
2025-01-20 08:38:05 +0000

    tailwind config

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100 text-blue-500">
+        <h1 className="text-4xl font-bold">Tailwind CSS is working!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
