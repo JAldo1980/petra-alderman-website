41f6a205d5f40001aef1491b859130293888df34
James Alderman <jamesalderman1980@gmail.com>
2025-01-28 13:46:11 +0000

    Research page added and updated

--- src/pages/Research.jsx
+++ src/pages/Research.jsx
@@ -1,10 +1,104 @@
 import React from "react";
+import Container from "../components/Container";
 
 const Research = () => {
   return (
-    <>
-      <h2>Research</h2>
-    </>
+    <div className="py-8">
+      <Container>
+        <div className="">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+            {/* Text Section */}
+            <div>
+              <h2 className="text-3xl font-bold mb-4">Research</h2>
+              <p className="mb-4">
+                As a researcher, I am deeply fascinated by the question of how
+                power is sustained and reproduced. I have explored this question
+                primarily in the context of authoritarian regimes, focusing
+                specifically on elections, nation branding and Southeast Asia.
+              </p>
+
+              <h2 className="text-2xl font-semibold mb-4">Elections</h2>
+              <p className="mb-4">
+                My research in this area explores the role elections play in
+                authoritarian regimes. Most contemporary authoritarian regimes
+                hold regular, even competitive elections, but authoritarian
+                elections are often manipulated to ensure the regime’s
+                continuous grip on power. There are many different ways in which
+                authoritarian regimes can manipulate electoral contests, ranging
+                from ballot box stuffing to banning of opposition parties, but I
+                am particularly interested in understanding how and why formally
+                independent election commissions contribute to electoral
+                manipulation in these contexts.
+              </p>
+              <p className="mb-4">
+                In my work, I argue that the establishment of formally
+                independent election commissions creates a set of challenges
+                that may become part of the problem why elections in
+                authoritarian regimes fail to deliver change in power. Delving
+                deep into the roles, responsibilities, powers, and
+                organisational structures of election commissions and their
+                relationships with different electoral stakeholders, I show how
+                formally independent election commission can become
+                authoritarian gatekeepers, hindering prospects for democracy in
+                some contexts.
+              </p>
+
+              <h2 className="text-2xl font-semibold mb-4">Nation Branding</h2>
+              <p className="mb-4">
+                My work in this area is inspired by the changing nature of
+                contemporary authoritarianism, both domestically and in its
+                interactions with the outside world. It brings together
+                scholarship in the disciplines of comparative politics,
+                international relations and critical geopolitics. Nation
+                branding is a popular practice among authoritarian regimes but
+                has attracted relatively little interest among politics and
+                international relations scholars.
+              </p>
+              <p className="mb-4">
+                The core argument of my research work is that nation branding
+                forms an integral part of state legitimation processes in
+                authoritarian regimes. Because of this, we need to stop seeing
+                nation branding as an externally oriented, business-derived
+                practice and instead understand it as an inherently
+                inward-looking promotional strategy aimed at shaping the social
+                attitudes and behaviours of the nation’s citizens in line with
+                the government’s domestic agenda and legitimation needs.
+              </p>
+
+              <h2 className="text-2xl font-semibold mb-4">Southeast Asia</h2>
+              <p className="mb-4">
+                Southeast Asia has a long history of authoritarian rule and
+                continues to be home to many different forms of
+                authoritarianism. This makes it a fascinating region to study.
+                By examining the different characteristics of Southeast Asian
+                authoritarianism, we can gain a better understanding of the
+                factors that enable authoritarianism to persist and even thrive
+                in this diverse region.
+              </p>
+              <p className="mb-4">
+                In this area, I am currently working on a research article that
+                examines the extent to which the US-China rivalry maps on the
+                domestic political attitudes of Thai citizens. The paper’s
+                findings will have important implications for our understanding
+                of how China’s rise affects popular attitude towards democracy
+                in Southeast Asia whilst demonstrating the need to understand
+                the effects of the US-China rivalry beyond focusing on the level
+                of the political elites.
+              </p>
+
+              <p className="mb-6">
+                Overall, my research contributes to the study of
+                authoritarianism by exploring a range of issues across different
+                countries and contexts. By gaining a comprehensive understanding
+                of how authoritarian power is sustained and reproduced, I hope
+                to inform efforts on how to promote democracy around the world
+                in more effective and culturally and context-sensitive ways.
+              </p>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
   );
 };
 
